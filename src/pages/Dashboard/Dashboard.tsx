5d36bbb3c6596823f4ed047e77322dc15a761aa0
Josenilsonfariasx <fariaslwork@gmail.com>
2023-10-14 15:36:17 -0300

    feat: adding page dashboard

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,32 @@
+import { Header } from "../../components/Header/Header";
+import { TechList } from "../../components/DriverList/DriverList";
+import style from "./style.module.scss";
+
+export interface user {
+  id: string;
+  name: string;
+  created_at?: string;
+  updated_at?: string;
+}
+export const Dashboard = () => {
+  const storedUserString = localStorage.getItem("@me2-user");
+  let user: user | null = null;
+
+  if (storedUserString) {
+    user = JSON.parse(storedUserString);
+  }
+
+  return (
+    <div className={style.div}>
+      <Header visible={true} />
+      <main className={style.main}>
+        <div className={style.user_}>
+          <div className="container">
+            <h2 className="title white">{user?.name}</h2>
+          </div>
+        </div>
+        <div className={style.info}><TechList /></div>
+      </main>
+    </div>
+  );
+};
