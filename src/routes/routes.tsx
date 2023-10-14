9353d33d189579e17ffdb00aa2914839b3032afa
Josenilsonfariasx <fariaslwork@gmail.com>
2023-10-14 18:32:01 -0300

    feat: refactor routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,16 +4,21 @@ import { SingIn } from "../pages/SingIn/singIn";
 import { Dashboard } from "../pages/Dashboard/Dashboard";
 import { ProtectedRoutes } from "../components/ProtectedRoutes/ProtectedRoutes";
 import { DriverProvider } from "../Providers/DriverContext";
+import { TransportProvider } from "../Providers/TransportContext";
 
 export const RoutesMain = () => {
   return (
     <AdminProvider>
-      <Routes>
-        <Route path="/" element={<SingIn />} />
-        <Route path="/home" element={<ProtectedRoutes />}>
-          <Route index element={<DriverProvider><Dashboard /></DriverProvider> } />
-        </Route>
-      </Routes>
+      <DriverProvider>
+        <TransportProvider>
+          <Routes>
+            <Route path="/" element={<SingIn />} />
+            <Route path="/home" element={<ProtectedRoutes />}>
+              <Route index element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </TransportProvider>
+      </DriverProvider>
     </AdminProvider>
   );
 };
