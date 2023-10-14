fb3abc8df7cc1c02c5d1c7ec0a2007eb8a99317a
Josenilsonfariasx <fariaslwork@gmail.com>
2023-10-14 04:07:00 -0300

    feat: adding routes

---
+++ src/routes/routes.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import { AdminProvider } from "../Providers/AdminContext";
+import { SingIn } from "../pages/singIn";
+
+export const RoutesMain = () => {
+  return (
+    <AdminProvider>
+      <Routes>
+        <Route path="/" element={<SingIn />} />
+      </Routes>
+    </AdminProvider>
+  );
+};
