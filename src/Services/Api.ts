5adc8da9ff01fff0ed702f469cc7309200e8edb5
Josenilsonfariasx <fariaslwork@gmail.com>
2023-10-15 04:34:28 -0300

    feat: adding link api server

--- src/Services/Api.ts
+++ src/Services/Api.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 export const Api = axios.create ({
-    baseURL: "http://localhost",
+    // baseURL: "http://localhost",
+    baseURL:"https://driver-management-platform-api.onrender.com",
     timeout: 8000,
 })
