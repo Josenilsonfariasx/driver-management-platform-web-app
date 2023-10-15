a4f1f49762a9e48b52dcf63db3f241cde3971db8
Josenilsonfariasx <fariaslwork@gmail.com>
2023-10-15 16:36:49 -0300

    style: fixing style header dashboard

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,22 +2,25 @@
 import { useNavigate } from "react-router-dom";
 import style from "./style.module.scss"
 import { toast } from "react-toastify";
+import { useDriverContext } from "../../Providers/DriverContext";
 // import { Link, useNavigate } from "react-router-dom"
 interface Header {
   visible?: boolean;
 }
 export const Header = ({visible}:Header) => {
+  const {setLoading}: any = useDriverContext();
     const navi = useNavigate()
     const logout = ()=>{
       localStorage.removeItem("@me2-token")
       localStorage.removeItem("@me2-user")
       navi("/")
+      setLoading(true)
       toast.success("Volte sempre")
     }
     return (
         <header className={(visible ? style.headerBtn : style.header)}>
             <div className="container">
-                <h1 className="title">Driver Management</h1>
+                <h1 className="title one">Driver Management</h1>
                 {visible?<button className="button variant" onClick={()=>logout() }>Sair</button>: null}
             </div>
         </header>
