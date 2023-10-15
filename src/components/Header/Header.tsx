3b5d2bb2b8f794034684bb9adacc1136337c043a
Josenilsonfariasx <fariaslwork@gmail.com>
2023-10-15 04:32:53 -0300

    feat: adding functionally logout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,24 @@
 // import { useUserContext } from "../../providers/UserContext"
+import { useNavigate } from "react-router-dom";
 import style from "./style.module.scss"
+import { toast } from "react-toastify";
 // import { Link, useNavigate } from "react-router-dom"
 interface Header {
   visible?: boolean;
 }
 export const Header = ({visible}:Header) => {
-    // const {user, logout} = useUserContext()
-    // const navi = useNavigate()
+    const navi = useNavigate()
+    const logout = ()=>{
+      localStorage.removeItem("@me2-token")
+      localStorage.removeItem("@me2-user")
+      navi("/")
+      toast.success("Volte sempre")
+    }
     return (
         <header className={(visible ? style.headerBtn : style.header)}>
             <div className="container">
                 <h1 className="title">Driver Management</h1>
-                {/* {visible ? (
-                    user ? (
-                        <button className="button variant" onClick={()=>logout() }>Sair</button>
-                    ):<button className="button variant" onClick={()=> navi("/")}>Voltar</button>
-                ): null} */}
+                {visible?<button className="button variant" onClick={()=>logout() }>Sair</button>: null}
             </div>
         </header>
     )
